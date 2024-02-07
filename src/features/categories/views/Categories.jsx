9369b6113b279f863d0adb72495a41ec953131dc
Dominik <spiderqwerty@gmail.com>
2024-02-07 22:37:01 +0100

    changed Categories page

--- src/features/categories/views/Categories.jsx
+++ src/features/categories/views/Categories.jsx
@@ -1,5 +1,20 @@
+import Post from '../../../components/Post/Post';
+import styles from './categories.module.css';
+import CreatePost from '../../../components/CreatePost/CreatePost'
+
 const Categories = () => {
-  return <div>Categories</div>;
+  const data = { 'id': '1', 'title': 'Title', 'description': 'Description', };
+
+  return (
+    <div className={styles.wrapper}>
+      <CreatePost/>
+      <Post className={styles.post} data={data}></Post>
+      <Post className={styles.post} data={data}></Post>
+      <Post className={styles.post} data={data}></Post>
+      <Post className={styles.post} data={data}></Post>
+      <Post className={styles.post} data={data}></Post>
+    </div >
+  );
 };
 
 export default Categories;
